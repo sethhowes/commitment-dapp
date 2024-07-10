076bd6fd71bd8b1bd2668dc9d9c0c70a4f8c14b3
Seth Howes <sethshowes@gmail.com>
2024-07-09 22:27:01 -0700

    chore: Update uploadSecrets.js for Polygon Amoy testnet

--- chainlink-functions/uploadSecrets.js
+++ chainlink-functions/uploadSecrets.js
@@ -2,10 +2,10 @@ const { SecretsManager } = require("@chainlink/functions-toolkit");
 const ethers = require("ethers");
 require('dotenv').config();
 
-const makeRequestMumbai = async () => {
+const makeRequestAmoy = async () => {
   // hardcoded for Polygon Mumbai
-  const routerAddress = "0x6E2dc0F9DB014aE19888F539E59285D2Ea04244C";
-  const donId = "fun-polygon-mumbai-1";
+  const routerAddress = "0xC22a79eBA640940ABB6dF0f7982cc119578E11De";
+  const donId = "fun-polygon-amoy-1";
   const gatewayUrls = [
     "https://01.functions-gateway.testnet.chain.link/",
     "https://02.functions-gateway.testnet.chain.link/",
